40418c4b0847b6b16ded41aabbf8098582a03947
Jakub Haliniak <jakubh.fake@gmail.com>
2023-11-23 21:52:54 +0100

    add loading for gifts list refresing

--- src/components/Gifts/GiftsList.tsx
+++ src/components/Gifts/GiftsList.tsx
@@ -6,6 +6,7 @@ export const GiftsList = () => {
     const [giftsList, setGiftsList] = useState<GiftEntity[] | null>(null);
 
     const refreshGifts = async () => {
+        setGiftsList(null);
         const res = await fetch (
             'http://localhost:3001/gift');
         const data = await res.json();
