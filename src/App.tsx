51c9eac814c32f23656725dace7799c5f6594893
Jakub Haliniak <jakubh.fake@gmail.com>
2023-11-26 17:15:34 +0100

    add new view GiftView

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css';
 import {GiftsList} from "./components/Gifts/GiftsList";
+import {GiftsView} from "./views/GiftsView";
 
 function App() {
   return (
     <div className="App">
-       <GiftsList/>
+       <GiftsView/>
     </div>
   );
 }
---
+++ src/views/GiftsView.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {GiftsList} from "../components/Gifts/GiftsList";
+
+export const GiftsView = () => (
+    <>
+        <GiftsList/>
+    </>
+);
