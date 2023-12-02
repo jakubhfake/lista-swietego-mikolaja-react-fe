8673427f0640a5f7cbe7487129d62fabfe6d09ce
Jakub Haliniak <jakubh.fake@gmail.com>
2023-12-02 14:09:17 +0100

    configure React Router

---
+++ src/views/TestView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {GiftsList} from "../components/Gifts/GiftsList";
+import {GiftAdd} from "../components/Gifts/GiftAdd";
+
+export const TestView = () => (
+    <>
+        <h1>
+            Test!
+        </h1>
+        </>
+);
