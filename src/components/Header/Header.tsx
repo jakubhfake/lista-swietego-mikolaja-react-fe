66846b78c74a9f4b82ac8606297652a8a291f1dd
Jakub Haliniak <jakubh.fake@gmail.com>
2023-12-05 06:04:31 +0100

    add NavLink in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import React from "react";
 
 export const Header = () => {
     return <>
         <h1>Santa Gifts App</h1>
         Menu:
-        <Link to={"/gift"}>Gifts</Link> |
+        <NavLink style={({isActive}) => ({color: isActive ? 'green' : 'red'})} to={"/gift"}>Gifts</NavLink> |
         <Link to={"/test"}>Test</Link> |
         <hr/>
     </>
